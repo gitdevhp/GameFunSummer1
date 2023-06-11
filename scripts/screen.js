1e73d81c78ab3d40288fbed521fe868e94473ba8
Jaden Cai <67614448+gitdevhp@users.noreply.github.com>
2023-06-11 17:17:12 -0500

    br

--- scripts/screen.js
+++ scripts/screen.js
@@ -1,3 +1,7 @@
+function gameSet(){
+    document.getElementById('pageHideShow').style.display='block';
+}
+
 function gamePlay() {
     console.log('arrived');
     document.getElementById('loaded').style.display='none';
