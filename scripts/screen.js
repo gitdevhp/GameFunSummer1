9877dbbce59c8c8c5e288901c9a7463cd57344d8
Jaden Cai <67614448+gitdevhp@users.noreply.github.com>
2023-06-12 16:00:03 -0500

    lol

--- scripts/screen.js
+++ scripts/screen.js
@@ -20,7 +20,7 @@ function gameSet(firstSce, secondSce){
             document.getElementById('splashText').style.display='none';
             document.getElementById('splashText').style.animationPlayState='initial';
             document.getElementById('splashText').style.animationPlayState='paused';
-        }, 1500);
+        }, 2500);
     }, 1500);
 }
 
