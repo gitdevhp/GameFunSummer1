f3872860fb0b739564140e80be2aadd2317e8a7a
Jaden Cai <67614448+gitdevhp@users.noreply.github.com>
2023-06-12 15:23:49 -0500

    ok

--- scripts/screen.js
+++ scripts/screen.js
@@ -1,13 +1,17 @@
 function gameSet(){
     console.log('ok');
     document.getElementById('pageHideShow').style.display='block';
-    document.getElementsByClassName('trans1').style.animationPlayState='running';
+    document.getElementsByClassName('trans1')[0].style.animationPlayState='running';
+    document.getElementsByClassName('trans1')[1].style.animationPlayState='running';
     setTimeout(() => {
         gamePlay();
     }, 3000);
     setTimeout(() => {
-        document.getElementsByClassName('trans1').style.animationPlayState='initial';
-        document.getElementsByClassName('trans1').style.animationPlayState='paused';
+        document.getElementsByClassName('trans1')[0].style.animationPlayState='initial';
+        document.getElementsByClassName('trans1')[1].style.animationPlayState='initial';
+        document.getElementsByClassName('trans1')[0].style.animationPlayState='paused';
+        document.getElementsByClassName('trans1')[1].style.animationPlayState='paused';
+
       }, 6000);
 }
 
