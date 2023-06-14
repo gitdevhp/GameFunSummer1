215c9ece71672e12d3aae7e13d0271883e58484d
Jaden Cai <67614448+gitdevhp@users.noreply.github.com>
2023-06-14 17:13:25 -0500

    fix

--- scripts/dragDrop.js
+++ scripts/dragDrop.js
@@ -1,7 +1,7 @@
-const selectCard = document.querySelector('#test');
+const selectCard = document.querySelector('.card');
 const droppedArea = document.querySelectorAll('.cardSect');
 
-selectCard.addEventListener('dragStart', dragStart);
+selectCard.addEventListener('dragstart', dragStart);
 
 droppedArea.forEach(placeArea => {
     droppedArea.addEventListener('dragover', dragOver);
