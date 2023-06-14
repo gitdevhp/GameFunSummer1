731f94e811401d3571c98fdf91d68747b4892024
Jaden Cai <67614448+gitdevhp@users.noreply.github.com>
2023-06-14 16:57:55 -0500

    please work

---
+++ scripts/dragDrop.js
@@ -0,0 +1,24 @@
+const selectCard = document.querySelectorAll('#test');
+const droppedArea = document.querySelectorAll('.cardSect');
+
+selectCard.addEventListener('dragStart', dragStart);
+
+droppedArea.forEach(placeArea => {
+    droppedArea.addEventListener('dragover', dragOver);
+    droppedArea.addEventListener('drop', dragDrop);
+});
+
+let beingDragged;
+
+function dragStart(dragged){
+    beingDragged=dragged.target;    
+}
+
+function dragOver(dragged) {
+    dragged.preventDefault();
+}
+
+function dragDrop(dragged) {
+    console.log(dragged.target);
+    dragged.target.append(beingDragged);
+}
